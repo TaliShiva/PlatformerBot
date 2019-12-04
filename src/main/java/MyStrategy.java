7699eecdfa303a24794ea899489dc0ac3de53892
TaliShiva <TaliShiva@mail.ru>
2019-12-05 01:07:03 +0600

    Подкрутил параметры

--- src/main/java/MyStrategy.java
+++ src/main/java/MyStrategy.java
@@ -10,8 +10,7 @@ public class MyStrategy {
 
     private static boolean isStupidShot(Unit unit, Unit nearestEnemy, Game game) {
 
-        if (unit.getWeapon() != null && distanceSqr(unit.getPosition(), nearestEnemy.getPosition()) > 5 &&
-                unit.getWeapon() != null && nearestEnemy.getWeapon() != null) {
+        if (unit.getWeapon() != null && unit.getWeapon().getTyp() == WeaponType.ROCKET_LAUNCHER && distanceSqr(unit.getPosition(), nearestEnemy.getPosition()) > 30) {
             return (game.getLevel().getTiles()[(int) (unit.getPosition().getX() - 1)][(int) (unit.getPosition().getY())] == Tile.WALL) ||
                     (game.getLevel().getTiles()[(int) (unit.getPosition().getX() + 1)][(int) (unit.getPosition().getY())] == Tile.WALL) ||
                     (game.getLevel().getTiles()[(int) (unit.getPosition().getX() - 1)][(int) (unit.getPosition().getY())] == Tile.WALL) ||
@@ -22,6 +21,19 @@ public class MyStrategy {
                     (game.getLevel().getTiles()[(int) (unit.getPosition().getX())][(int) (unit.getPosition().getY() + 1)] == Tile.WALL) ||
                     (game.getLevel().getTiles()[(int) (unit.getPosition().getX())][(int) (unit.getPosition().getY() - 1)] == Tile.WALL);
         }
+        if (unit.getWeapon() != null && unit.getWeapon().getTyp() == WeaponType.ASSAULT_RIFLE && distanceSqr(unit.getPosition(), nearestEnemy.getPosition()) > 20) {
+            return (game.getLevel().getTiles()[(int) (unit.getPosition().getX() - 1)][(int) (unit.getPosition().getY())] == Tile.WALL) ||
+                    (game.getLevel().getTiles()[(int) (unit.getPosition().getX() + 1)][(int) (unit.getPosition().getY())] == Tile.WALL) ||
+                    (game.getLevel().getTiles()[(int) (unit.getPosition().getX() - 1)][(int) (unit.getPosition().getY())] == Tile.WALL) ||
+                    (game.getLevel().getTiles()[(int) (unit.getPosition().getX() + 1)][(int) (unit.getPosition().getY() - 1)] == Tile.WALL) ||
+                    (game.getLevel().getTiles()[(int) (unit.getPosition().getX() - 1)][(int) (unit.getPosition().getY() - 1)] == Tile.WALL) ||
+                    (game.getLevel().getTiles()[(int) (unit.getPosition().getX() + 1)][(int) (unit.getPosition().getY() + 1)] == Tile.WALL) ||
+                    (game.getLevel().getTiles()[(int) (unit.getPosition().getX() - 1)][(int) (unit.getPosition().getY() + 1)] == Tile.WALL) ||
+                    (game.getLevel().getTiles()[(int) (unit.getPosition().getX())][(int) (unit.getPosition().getY() + 1)] == Tile.WALL) ||
+                    (game.getLevel().getTiles()[(int) (unit.getPosition().getX())][(int) (unit.getPosition().getY() - 1)] == Tile.WALL);
+        }
+
+
         return false;
     }
 
@@ -40,7 +52,7 @@ public class MyStrategy {
                 if (nearestEnemy == null || distanceSqr(unit.getPosition(),
                         other.getPosition()) < distanceSqr(unit.getPosition(), nearestEnemy.getPosition())) {
                     nearestEnemy = other;
-                    System.out.println("nearestEnemy = other");
+//                    System.out.println("nearestEnemy = other");
                 }
             }
         }
@@ -61,25 +73,25 @@ public class MyStrategy {
             targetPos = nearestEnemy.getPosition(); // если есть оружие, то пиздуй к врагу
         }
 
-        targetPos = savePlayer(unit, game, nearestEnemy, targetPos);
+
 
         //беги за базукой если есть возможность
         targetPos = goToRocketIfCan(unit, game, targetPos);
-
+        targetPos = savePlayer(unit, game, nearestEnemy, targetPos);
         //  PathFinder pf = new PathFinder();
         //   pf.getPath(unit.getPosition(),nearestWeapon.getPosition(),game.getLevel());
 
         debug.draw(new CustomData.Log("Target pos: " + targetPos));
         Vec2Float debugUnitPoint = new Vec2Float((float) unit.getPosition().getX(), (float) unit.getPosition().getY());
-        Vec2Float debugTargetPoint = new Vec2Float((float) targetPos.getX(), (float) targetPos.getY());
-        debug.draw(new CustomData.Line(debugUnitPoint, debugTargetPoint, 0.2f, new ColorFloat(100, 0, 0, 100)));
+
 
 
         Vec2Double aim = new Vec2Double(0, 0);
         if (nearestEnemy != null) {
             aim = new Vec2Double(nearestEnemy.getPosition().getX() - unit.getPosition().getX(),
                     nearestEnemy.getPosition().getY() - unit.getPosition().getY());
-            System.out.printf("aim x=%f;y=%f", aim.getX(), aim.getY());
+            Vec2Float debugAimPoint = new Vec2Float((float) aim.getX(), (float) aim.getY());
+            debug.draw(new CustomData.Line(debugUnitPoint,debugAimPoint,0.2f, new ColorFloat(0, 100, 0, 100)));
         }
 
 
@@ -92,14 +104,18 @@ public class MyStrategy {
                 .getTiles()[(int) (unit.getPosition().getX() - 1)][(int) (unit.getPosition().getY())] == Tile.WALL) {
             jump = true;
         }
+        if(distanceSqr(unit.getPosition(),nearestEnemy.getPosition()) <= 20){
+            jump = true;
+        }
 
-        targetPos = backFromEnemy(nearestEnemy, unit, game);
+        targetPos = backFromEnemy(targetPos, nearestEnemy, unit, game);
 
         UnitAction action = new UnitAction();
         double signum = Math.signum(targetPos.getX() - unit.getPosition().getX());
         action.setVelocity(signum * game.getProperties().getUnitMaxHorizontalSpeed());
         action.setJump(jump);
-        action.setJumpDown(!jump);
+
+        jumpDownIfNeedIt(unit, game, targetPos, action);
 
         action.setAim(aim);
         action.setShoot(true);
@@ -118,20 +134,30 @@ public class MyStrategy {
         }
 
         action.setPlantMine(false);
+
+        Vec2Float debugTargetPoint = new Vec2Float((float) targetPos.getX(), (float) targetPos.getY());
+        debug.draw(new CustomData.Line(debugUnitPoint, debugTargetPoint, 0.2f, new ColorFloat(100, 0, 0, 100)));
+
         return action;
     }
 
-    private Vec2Double backFromEnemy(Unit nearestEnemy, Unit unit, Game game) {
-        Vec2Double targetPos = new Vec2Double();
+    private void jumpDownIfNeedIt(Unit unit, Game game, Vec2Double targetPos, UnitAction action) {
+        if(game.getLevel().getTiles()[(int)unit.getPosition().getX()][(int)unit.getPosition().getY()-1] == Tile.PLATFORM &&
+        targetPos.getY()<unit.getPosition().getY()) {
+            action.setJumpDown(true);
+        }else {
+            action.setJumpDown(false);
+        }
+    }
+
+    private Vec2Double backFromEnemy(Vec2Double targetPos, Unit nearestEnemy, Unit unit, Game game) {
         Vec2Double enemyPosition = nearestEnemy.getPosition();
         Vec2Double unitPosition = unit.getPosition();
-        if (distanceSqr(unitPosition, enemyPosition) <= 15) {
+        if (distanceSqr(unitPosition, enemyPosition) <= 18) {
             if (enemyPosition.getX() > unitPosition.getX()) {// враг правее
-                targetPos.setX(enemyPosition.getX() - 20);
-                targetPos.setY(enemyPosition.getY() + 20);
+                targetPos.setX(enemyPosition.getX() - 39);
             } else if (enemyPosition.getX() < unitPosition.getX()) {//враг левее
-                targetPos.setX(enemyPosition.getX() + 20);
-                targetPos.setY(enemyPosition.getY() + 20);
+                targetPos.setX(enemyPosition.getX() + 39);
             }
         }
         return targetPos;
@@ -140,7 +166,7 @@ public class MyStrategy {
     private Vec2Double savePlayer(Unit unit, Game game, Unit nearestEnemy, Vec2Double targetPos) {
         if (nearestEnemy != null && (nearestEnemy.getHealth() - unit.getHealth()) >= 23 || unit.getHealth() <= 45) {
             for (LootBox lootBox : game.getLootBoxes()) {
-                if (lootBox.getItem() instanceof Item.HealthPack && distanceSqr(unit.getPosition(), lootBox.getPosition()) <= 30) { // если лутбокс это аптечка, то
+                if ((lootBox.getItem() instanceof Item.HealthPack) && distanceSqr(unit.getPosition(), lootBox.getPosition()) <= 75) { // если лутбокс это аптечка, то
                     targetPos = lootBox.getPosition();
                 }
             }
